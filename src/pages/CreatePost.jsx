b0491b61034508c0604cbfe7017e8b183b159440
Mehedi1324 <you@example.com>
2023-05-19 17:48:56 +0600

    add img generatinig post url

--- src/pages/CreatePost.jsx
+++ src/pages/CreatePost.jsx
@@ -67,15 +67,18 @@ const CreatePost = () => {
     if (form.prompt) {
       try {
         setGeneratingImg(true);
-        const response = await fetch('http://localhost:1000/api/dalle', {
-          method: 'POST',
-          headers: {
-            'Content-Type': 'application/json',
-          },
-          body: JSON.stringify({
-            prompt: form.prompt,
-          }),
-        });
+        const response = await fetch(
+          'https://dall-e-server-delta.vercel.app/api/dalle',
+          {
+            method: 'POST',
+            headers: {
+              'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+              prompt: form.prompt,
+            }),
+          }
+        );
 
         const data = await response.json();
         setForm({ ...form, photo: `data:image/jpeg;base64,${data.photo}` });
